7598d579b79b34fa281f77a6a45bf7b8b913a3aa
Pedro <p.miraspc35@gmail.com>
2025-11-03 19:45:28 +0100

    Add login page and update navigation links
    Added login.php and its corresponding CSS for the login page. Updated header.php navigation links to use PHP files and point to the new login page. Included index.css in header.php and footer.php for consistent styling.

--- public/header.php
+++ public/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/index.css">
     <title>Acho Q Como</title>
 </head>
 <body>
@@ -16,10 +17,10 @@
 
         <nav class="nav">
             <ul>
-                <li><a href="index.html">Inicio</a></li>
+                <li><a href="index.php">Inicio</a></li>
                 <li><a href="restaurantes.html">Restaurantes</a></li>
-                <li><a href="registro.html">Registro</a></li>
-                <li><a href="login.html">Login</a></li>
+                <li><a href="registro.php">Registro</a></li>
+                <li><a href="./public/login.php">Login</a></li>
                 <li><a href="carrito.html"><i class="fa-solid fa-cart-shopping"></i></a></li>
             </ul>
         </nav>
